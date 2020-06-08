8e96f967eced16809ea8d82c1251c01523758e33
Paulami Bhattacharya <paulamibhattacharya@Paulamis-MacBook-Air.local>
2020-06-08 20:16:07 +0530

    edited

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,16 +1,6 @@
-fizzbuzz n = 
-    if mod n 15 == 0
-        then "FizzBuzz"
-    else if mod n 5 == 0
-        then "Fizz"
-    else if mod n 3 == 0
-        then "Buzz"
-    else show n
-        
+fb = zipWith (++) (cycle["","","FIZZ"]) (cycle["","","","","BUZZ"])
+pick a b = if null a then b else a
+fizzbuzz = zipWith pick fb $ map show [1..]
+
 main = do
-		print $ fizzbuzz 30
-		print $ fizzbuzz 10
-		print $ fizzbuzz 15
-		print $ fizzbuzz 21
-		print $ fizzbuzz 81
-		print $ fizzbuzz 49
+    print $ take 30 fizzbuzz
